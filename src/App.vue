0891a1f93b7b32eba5e5bdc4d345e23ffe0128f6
Arianna Basilone <abasilon@uwo.ca>
2020-09-16 01:20:14 -0400

    initial

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <h1>Internship Signup Form</h1>
+    <div class="line"></div>
+    <SignUp msg="Prepare for your career with a Project Management, Web-Development, Graphic design, or Digital Marketing internship at Northern."/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
+</template>
+
+<script>
+import SignUp from './components/SignUp.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SignUp
+    // HelloWorld
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Merriweather&family=Montserrat:wght@400;700&display=swap');
+#app {
+  /* font-family: Avenir, Helvetica, Arial, sans-serif; */
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  /* color: #2c3e50; */
+  /* margin-top: 60px;   */
+
+  font-family: 'Merriweather', sans-serif;
+  color: white;
+  margin-left:10%;
+  margin-right:10%;
+}
+html{
+  background-color: #282828;
+}
+h1{
+  font-family: 'Montserrat', sans-serif;
+  font-size: 65px;
+  text-transform: uppercase;
+  color: white;
+  margin-top: 10%;
+}
+
+.line{
+  border-bottom: 10px solid #D02035;
+  width:30%;
+}
+</style>
---
+++ src/components/SignUp.vue
@@ -0,0 +1,102 @@
+<template>
+    <div class="content">
+        <div v-show="sign">
+            <p>{{ msg }}</p>
+            <form v-on:submit.prevent="xyz()">
+            <div class="formFields">
+            <input class="email" v-model="email" type="email" name="emailAddr" placeholder="Your Email Address *" required :disabled="tf">
+            <input list="intr" v-model="intr" name="interest" placeholder="Your Interests" :disabled="tf">
+            <datalist id="intr">
+                <option value="Developement"></option>
+                <option value="Project Management"></option>
+                <option value="Graphic Design"></option>
+                <option value="Marketing"></option>
+            </datalist>
+            </div>
+            <button id="signUpButton" type="submit" :disabled="tf">{{buttonMsg}}</button>
+            </form>
+        </div>
+
+        <div v-show="thank">
+            <h3>Thanks for your interest!</h3>
+            <p>We will review your application and contact you for additional information should your background and experience meet the requirements of one of our openings</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'SignUp',
+  props:{
+      msg : String
+  },
+  data(){
+      return{
+          sign: true,
+          thank:false,
+          buttonMsg: "Sign Up Now ▶",
+          tf: false,
+          email: '',
+          intr: ''
+      }
+  },
+  methods:{
+      xyz: function(){
+          console.log("Email: "+this.email+"\nInterest: "+this.intr);
+          this.buttonMsg="Submitting...";
+          this.tf=true;
+          setTimeout(() => {this.sign=false; this.thank=true}, 2000);
+          
+      }
+  }
+}
+</script>
+
+<style scoped>
+.content{
+    margin-top: 5%;
+}
+.formFields{
+    display: flex;
+    flex-direction: row;
+    margin-top: 5%;
+}
+input{
+    width:49%;
+    display: block;
+    padding: 2%;
+    font-style: italic;
+    font-size: 16px;
+}
+
+input:disabled, input.email:disabled{
+    background-color: white;
+}
+
+.email{
+    margin-right:2%;
+}
+input.email:focus:invalid {
+    border-color:#D02035;
+}
+
+#signUpButton{
+    margin-top: 2%;
+    padding: 2%;
+    width: 100%;
+    font-weight: bold;
+    font-size: 20px;
+    font-style: normal;
+    color: white;
+    background-color: #D02035;
+    border: none;
+    cursor: pointer;
+    border-radius: 5px;
+}
+
+h3{
+    font-weight: bold;
+    font-size: 35px;
+}
+
+</style>
